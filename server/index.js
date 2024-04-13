d03933817bff533ebc4e0ee6e3124241e8609284
Heitor Avles <twitchmepaga@gmail.com>
2024-04-13 14:48:39 -0300

    primeiro comm

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+
+app.get("/", (req, res) => res.sendFile("C:/Users/heito/OneDrive/Ambiente de Trabalho/Programação/Node/page 1/public/index.html"));
+
+app.listen(process.env.port, () => {
+  console.log('Servidor Ligado');
+});
