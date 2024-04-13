576a7837f90fa7eb06a91d8e4a6d72c877c0de59
Heitor Avles <twitchmepaga@gmail.com>
2024-04-13 15:28:34 -0300

    teste3

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 
-app.get("/", (req, res) => res.sendFile("C:/Users/heito/OneDrive/Ambiente de Trabalho/Programação/Node/page 1/public/index.html"));
+app.get("/", (req, res) => res.sendFile("C:/Users/heito/OneDrive/Ambiente de Trabalho/Programação/Node/page-1/public/index.html"));
 
 
 app.listen(process.env.port, () => {
